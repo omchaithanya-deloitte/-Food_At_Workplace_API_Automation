55cb84aeb18cbf3dd5fb2e640a03ea57ed6e92dc
neerajdhurandher-deloitte <101085508+neerajdhurandher-deloitte@users.noreply.github.com>
2022-04-25 09:51:27 +0530

    fix https validation

--- src/test/java/resources/baseClass/BaseClass.java
+++ src/test/java/resources/baseClass/BaseClass.java
@@ -1,5 +1,7 @@
 package resources.baseClass;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import resources.helperclasses.Utils;
 import testAutomationListner.ExtentReportListener;
@@ -7,6 +9,9 @@ import testAutomationListner.ExtentReportListener;
 @Listeners (ExtentReportListener.class)
 public class BaseClass extends Utils {
 
-
+    @BeforeMethod
+    public void inti(){
+        RestAssured.useRelaxedHTTPSValidation();
+    }
 
 }
