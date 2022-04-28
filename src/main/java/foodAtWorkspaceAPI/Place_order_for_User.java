bfb7408fbbe123d5238d6e93c1d1a3e2932acb9a
naratreddy <naratreddy@deloitte.com>
2022-04-28 09:30:25 +0530

    added post order for Order_Controller

--- src/main/java/foodAtWorkspaceAPI/Place_order_for_User.java
+++ src/main/java/foodAtWorkspaceAPI/Place_order_for_User.java
@@ -1,19 +1,15 @@
 package foodAtWorkspaceAPI;
 
 import PojoClasses.Place_order;
-import PojoClasses.order_data;
 import org.json.JSONObject;
 
 public class Place_order_for_User {
     public static JSONObject sendRequest(String email, int amount, int tableNo){
         Place_order order = new Place_order();
-        order_data data=new order_data();
+
         order.setEmail(email);
         order.setamount(amount);
         order.settableNo(tableNo);
-        data.setitem_id1("2");
-        data.setitem_id2("1");
-        order.setdata(data);
 
         JSONObject jsonObject=new JSONObject();
         jsonObject.put("email",email);
